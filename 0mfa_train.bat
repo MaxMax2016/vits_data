e7a3e9331d024d2048ef4fe09a686a675534023e
IceKyrin <40847087+IceKyrin@users.noreply.github.com>
2022-09-22 08:06:16 +0800

    Add files via upload

---
+++ 0mfa_train.bat
@@ -0,0 +1,2 @@
+mfa_align.exe ./qiu/wavs/train ./mandarin_pinyin.dict ./mandarin.zip ./qiu/tr
+pause
---
+++ 1mfa_val.bat
@@ -0,0 +1,2 @@
+mfa_align.exe ./qiu/wavs/val ./mandarin_pinyin.dict ./mandarin.zip ./qiu/va
+pause
